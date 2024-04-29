42272315ee20959a45dbed93aac133f10247cd2d
Hunter Wyatt <huntercwyatt@gmail.com>
2024-04-29 08:03:32 -0600

    update landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <div className="h-full absolute inset">
-        <video autoPlay muted loop className="object-cover w-full h-full">
-          <source src="mountains.mp4" type="video/mp4" />
-        </video>
-      </div>
-      <div className="absolute inset-0 bg-overlay"></div>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-4 items-center">
+    <main className="min-h-screen flex flex-col justify-center items-center bg-brand p-8">
+      <div className="flex flex-col gap-4 items-center">
         <Image
           src="/logo.png"
           width={500}
